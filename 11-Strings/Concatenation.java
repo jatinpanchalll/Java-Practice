e81cb33129538e9c598d969d284118fcdefce2e2
Jatin Panchal <panchalj439@gmail.com>
2026-06-27 06:48:36 +0530

    Added String Programs

---
+++ 11-Strings/Concatenation.java
@@ -0,0 +1,10 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String FirstName = "Jatin";
+        String LastName = "Panchal";
+
+
+        String FullName = FirstName +" "+ LastName ;
+        System.out.println(FullName);
+    }
+}
---
+++ 11-Strings/StringLength.java
@@ -0,0 +1,6 @@
+public class StringLength{
+    public static void main(String[] args) {
+        String name = "Jatin Panchal";
+        System.out.println(name.length());
+    }
+}
