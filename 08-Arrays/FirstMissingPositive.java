27152232eec32971bc53d1087d55e60c51a37784
Jatin Panchal <panchalj439@gmail.com>
2026-07-21 23:13:18 +0530

    Added Array Problems

---
+++ 08-Arrays/FirstMissingPositive.java
@@ -0,0 +1,35 @@
+public class FirstMissingPositive {
+
+    public static int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+
+        // Place each number at its correct index
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 &&
+                   nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        // Find the first missing positive
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {3, 4, -1, 1};
+
+        System.out.println("First Missing Positive: " + firstMissingPositive(nums));
+    }
+}
